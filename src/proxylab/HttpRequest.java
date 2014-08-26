831de0d0ca80cdb414ad7368ce62e47adabcf275
victormelo <victorklbr@gmail.com>
2014-08-26 19:27:44 -0300

    Initial project structure

---
+++ src/proxylab/HttpRequest.java
@@ -0,0 +1,108 @@
+package proxylab;
+/**
+ * HttpRequest - HTTP request container and parser
+ *
+ * $Id: HttpRequest.java,v 1.2 2003/11/26 18:11:53 kangasha Exp $
+ *
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class HttpRequest {
+	/** Help variables */
+	final static String CRLF = "\r\n";
+	final static int HTTP_PORT = 80;
+	/** Store the request parameters */
+	String method;
+	String URI;
+	String version;
+	String headers = "";
+	/** Server and port */
+	private String host;
+	private int port;
+
+	/** Create HttpRequest by reading it from the client socket */
+	public HttpRequest(BufferedReader from) {
+		String firstLine = "";
+		String[] tmp = null;
+		try {
+			firstLine = from.readLine();
+		} catch (IOException e) {
+			System.out.println("Error reading request line: " + e);
+		}
+		
+		try {
+			tmp = firstLine.split(" ");
+			method = tmp[0];
+			URI = tmp[1];
+			version = tmp[2];
+			if(URI.startsWith("http://")) {
+				String[] tmpURI = URI.split("http://");
+				URI = tmpURI[1].substring(tmpURI[1].indexOf("/"));
+			}
+			if (!(method.equals("GET") || method.equals("POST") || method.equals("HEAD"))) {
+				System.out.println("Error: Method not GET, POST or HEAD");
+			}
+		} catch(Exception e) {
+			System.out.println("Error: first line null;");
+		}
+
+		try {
+			String line = from.readLine();
+			while (line.length() != 0) {
+				headers += line + CRLF;
+				/* We need to find host header to know which server to
+				 * contact in case the request URI is not complete. */
+				if (line.startsWith("Host:")) {
+					tmp = line.split(" ");
+					if (tmp[1].indexOf(':') > 0) {
+						String[] tmp2 = tmp[1].split(":");
+						host = tmp2[0];
+						
+						port = Integer.parseInt(tmp2[1]);
+					} else {
+						host = tmp[1];
+						
+						port = HTTP_PORT;
+					}
+				}
+				line = from.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading from socket: " + e);
+			return;
+		}
+//		System.out.println("Host to contact is: " + host + " at port " + port);
+	}
+
+	/** Return host for which this request is intended */
+	public String getHost() {
+		return host;
+	}
+
+	/** Return port for server */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * Convert request into a string for easy re-sending.
+	 */
+	public String toString() {
+		String req = "";
+
+		req = method + " " + URI + " " + version + CRLF;
+		req += headers;
+		/* This proxy does not support persistent connections */
+		req += "Connection: close" + CRLF;
+		req += CRLF;
+
+		return req;
+	}
+	
+	public String log() {
+		return "[REQUEST] URI requested is: " + URI + " method is: " + method;
+	}
+}
---
+++ src/proxylab/ThreadHandler.java
@@ -0,0 +1,73 @@
+package proxylab;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ThreadHandler extends Thread {
+
+	Socket client = null;
+
+	public ThreadHandler(Socket c) {
+		this.client = c;
+
+	}
+	public void run() {
+		Socket server = null;
+		HttpRequest request = null;
+		HttpResponse response = null;
+
+		/* Process request. If there are any exceptions, then simply
+		 * return and end this request. This unfortunately means the
+		 * client will hang for a while, until it timeouts. */
+
+		/* Read request */
+		try {
+			BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			request = new HttpRequest(fromClient);
+			System.out.println(request.log());
+
+		} catch (IOException e) {
+			System.out.println("Error reading request from client: " + e);
+			return;
+		}
+		/* Send request to server */
+		try {
+			/* Open socket and write request to socket */
+			server = new Socket(request.getHost(), request.getPort());
+			DataOutputStream toServer = new DataOutputStream(server.getOutputStream());
+			toServer.writeBytes(request.toString());
+			/* Fill in */
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: " + request.getHost());
+			System.out.println(e);
+			return;
+		} catch (IOException e) {
+			System.out.println("Error writing request to server: " + e);
+			return;
+		}
+		/* Read response and forward it to client */
+		try {
+			DataInputStream fromServer = new DataInputStream(server.getInputStream());
+			response = new HttpResponse(fromServer);
+			DataOutputStream toClient = new DataOutputStream(client.getOutputStream());
+			toClient.write(response.toString().getBytes());
+			toClient.write(response.body);
+			/* Write response to client. First headers, then body */
+			System.out.println(response.log(request.URI));
+			
+			client.close();
+			server.close();
+			/* Insert object into the cache */
+			/* Fill in (optional exercise only) */
+		} catch (IOException e) {
+			System.out.println("Error writing response to client: " + e);
+		}
+	}
+
+
+}
